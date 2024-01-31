dc18c8e4d9c6b8debeaa2a90197bb2ea6cbc4dc4
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-01-31 09:25:33 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -8,7 +8,8 @@
 // Credits: [Is your code based on an example in the book, in class, or something else?
 //            Reminder: you should never take code from the Internet or another person
 
-import java.util.Scanner;
+import java.util.*;
+import java.text.*;
 
 class Lab2 {
     public static void main(String[] args) {
@@ -19,9 +20,15 @@ class Lab2 {
         System.out.println("This program generates 10 lottery tickets.");
         
         System.out.print("What's your name? ");
-
         String customerName = input.nextLine();
 
-
+        DecimalFormat df = new DecimalFormat("00");
+        for (int i = 1: i <= 10; i++){
+            for (int j = 1; i <= 6; j++){
+                String rand = df.format((int) (Math.random() * 100));
+                System.out.print(rand);
+            }
+            System.out.println();
+        }
     }
 }
