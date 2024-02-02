5a8b121d25ebc93276d23b4ba1730b526a568cff
cyrusmccollim <106127824+cyrusmccollim@users.noreply.github.com>
2024-02-01 20:03:50 -0500

    Update Lab2.java

--- Lab2.java
+++ Lab2.java
@@ -11,26 +11,39 @@ import java.text.*;
 
 class Lab2 {
     public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat("00");
         Scanner input = new Scanner(System.in);
-
+        DecimalFormat df = new DecimalFormat("00");
+        
+        int[] winningNumbers = new int[]{3, 5, 16, 58, 59, 11};
         long prize = 225938745L;
+        double playAmount, winAmount;
+        String customerName;
 
-        System.out.println("CS 212 - Lab 2");
-        System.out.println("This program generates 10 lottery tickets.");
+        System.out.println("CS 212 - Lab 2 \nThis program generates 10 lottery tickets.");
         
         System.out.print("What's your name? ");
-        String customerName = input.nextLine();
+        customerName = input.nextLine();
 
-        System.out.println("Here is the ticket: ");
+        System.out.print("What's your play amount? $");
+        winAmount = playAmount = input.nextDouble();
+
+        System.out.println("Great, " + customerName + " wants to play $" + playAmount);
+
+        System.out.println("Here are the tickets: ");
         for (int i = 1; i <= 10; i++){
             for (int j = 1; j <= 6; j++){
-                String rand = df.format((int) (Math.random() * 100));
-                System.out.print(rand + " ");
+                int rand = (int) (Math.random() * 100);
+                if (winningNumbers.contains(rand))
+                    winAmount = Math.pow(winAmount, 1.75);
+                System.out.print(df.format(rand) + " ");
             }
             System.out.println();
         }
         
-        System.out.println("-----------------\n Good luck " + customerName + "!\n Estimated Jackpot:\n $" + prize + "\n -----------------");
+        System.out.println("-----------------" +
+                           "\n Good luck " + customerName + "!" +
+                           "\nPotential winnings: $" + prize +
+                           "\nYour winnings: $" + winAmount + 
+                           "\n -----------------");
     }
 }
